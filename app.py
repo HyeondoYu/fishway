535fac3c5a201450593bf33df0ff0c8cdeb62cee
yhd-mac <gusehx7191@gmail.com>
2025-07-16 11:37:15 +0900

    led

--- app.py
+++ app.py
@@ -2,8 +2,18 @@ from flask import Flask, render_template, Response
 from flask_socketio import SocketIO
 from camera import generate_frames
 from control import handle_command
+import Rpi.GPIO as GPIO
 
 app = Flask(__name__)
+
+LED_UP_PIN = 17
+LED_DOWN_PIN = 2
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(LED_UP_PIN, GPIO.OUT)
+GPIO.setup(LED_DOWN_PIN, GPIO.OUT)
+GPIO.output(LED_UP_PIN, GPIO.LOW)
+GPIO.output(LED_DOWN_PIN, GPIO.LOW)
 socketio = SocketIO(app)
 
 @app.route('/')
@@ -18,6 +28,28 @@ def video_feed():
 @socketio.on('control')
 def handle_control(command):
     handle_command(command)
+
+@app.route('/led/up/on')
+def led_up_on():
+    GPIO.output(LED_DOWN_PIN, GPIO.LOW)
+    GPIO.output(LED_UP_PIN, GPIO.HIGH)
+    return 'LED UP ON'
+
+@app.route('/led/up/off')
+def led_up_off():
+    GPIO.output(LED_UP_PIN, GPIO.LOW)
+    return 'LED UP OFF'
+
+@app.route('/led/down/on')
+def led_down_on():
+    GPIO.output(LED_UP_PIN, GPIO.LOW)
+    GPIO.output(LED_DOWN_PIN, GPIO.HIGH)
+    return 'LED DOWN ON'
+
+@app.route('/led/down/off')
+def led_down_off():
+    GPIO.output(LED_DOWN_PIN, GPIO.LOW)
+    return 'LED DOWN OFF'
 
 if __name__ == '__main__':
     socketio.run(app, host='0.0.0.0', port=5000)
