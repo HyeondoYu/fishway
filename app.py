a2e527174a7349215ad566875d036e62cffb4c10
HyeondoYu <100543484+HyeondoYu@users.noreply.github.com>
2025-07-23 11:29:01 +0900

    Update app.py

--- app.py
+++ app.py
@@ -24,4 +24,4 @@ def video_feed():
                     mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__ == '__main__':
-    run(app, host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000)
